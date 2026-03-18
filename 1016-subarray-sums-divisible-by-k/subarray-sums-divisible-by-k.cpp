0f5ce773b47632caad091946a4e658a45ff76ddf
Sana Perween <sana602perween@gmail.com>
2026-03-18 15:49:44 +0530

    Time: 15 ms (26.46%) | Memory: 35.6 MB (48.13%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.cpp
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.cpp
@@ -4,15 +4,15 @@ public:
         unordered_map<int, int> mp;
         mp[0] = 1;
 
-        int sum=0, count=0;
+        int sum=0,count=0;
 
         for(int num:nums) {
             sum +=num;
 
-            int rem = sum % k;
-            if(rem < 0) rem += k;
+            int rem=sum % k;
+            if(rem<0)rem+=k;
 
-            count += mp[rem];
+            count +=mp[rem];
             mp[rem]++;
         }
 
